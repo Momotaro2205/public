ecd398280eda8f38ccec4052f00314af879d2d24
Momotaro2205 <168008425+Momotaro2205@users.noreply.github.com>
2024-06-17 01:15:59 +0300

    login.php, User.php, router.php

---
+++ zadanie №20/User.php
@@ -0,0 +1,76 @@
+<?php
+
+class User
+{
+  private $name;
+  private $lastname;
+  private $email;
+  private $id;
+
+  function __construct($id, $name, $lastname, $email)
+  {
+    $this->id = $id;
+    $this->name = $name;
+    $this->lastname = $lastname;
+    $this->email = $email;
+  }
+
+  function getId()
+  {
+    return $this->id;
+  }
+  function getName()
+  {
+    return $this->name;
+  }
+  function getLastname()
+  {
+    return $this->lastname;
+  }
+  function getEmail()
+  {
+    return $this->email;
+  }
+
+  //Статический метод добавления пользователя в бд
+  static function addUser($name, $lastname, $email, $pass)
+  {
+    global $mysqli;
+
+    $email = mb_strtolower(trim($email));
+    $pass = trim($pass);
+    $pass = password_hash($pass, PASSWORD_DEFAULT);
+
+    $result = $mysqli->query("SELECT * FROM `users` WHERE `email`='$email'");
+
+    if ($result->num_rows != 0) {
+      return json_encode(["result" => "exist"]);
+    } else {
+      $mysqli->query("INSERT INTO `users`(`name`, `lastname`, `email`, `pass`) VALUES ('$name', '$lastname', '$email', '$pass')");
+      return json_encode(["result" => "success"]);
+    }
+  }
+  // статический метод авторизации пользователя
+  static function authUser($email, $pass)
+  {
+    session_start();
+    global $mysqli;
+
+    $email = mb_strtolower(trim($email));
+    $pass = trim($pass);
+
+    $authResult = $mysqli->query("SELECT * FROM `users` WHERE `email`='$email'");
+    $authResult = $authResult->fetch_assoc();
+
+    if (password_verify($pass, $authResult["pass"])) {
+      return json_encode(["authResult" => "authorized"]);
+
+      $_SESSION["id"] = $authResult["id"];
+      $_SESSION["email"] = $authResult["email"];
+      $_SESSION["name"] = $authResult["name"];
+      $_SESSION["lastname"] = $authResult["lastname"];
+    } else {
+      return json_encode(["authResult" => "access danied"]);
+    }
+  }
+}
